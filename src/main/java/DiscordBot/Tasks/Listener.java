3f2be5184255ef9081c2219258c1ec4bf745f35f
Tormak9970 <63308171+Tormak9970@users.noreply.github.com>
2020-06-11 17:54:29 -0500

    worked on performance

--- src/main/java/DiscordBot/Tasks/Listener.java
+++ src/main/java/DiscordBot/Tasks/Listener.java
@@ -3,10 +3,15 @@ package DiscordBot.Tasks;
 import DiscordBot.Tasks.Moderation.ModerationRunner;
 import DiscordBot.Tasks.Music.MusicRunner;
 import DiscordBot.Tasks.RLMafia.RLMafiaRunner;
+import net.dv8tion.jda.api.events.ReadyEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Listener extends ListenerAdapter {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Listener.class);
 
     @Override
     public void onMessageReceived(MessageReceivedEvent event) {
@@ -27,5 +32,10 @@ public class Listener extends ListenerAdapter {
             CommandsRunner.passEvent(event);
         }
 
+    }
+
+    @Override
+    public void onReady(@NotNull ReadyEvent event){
+        LOGGER.info("{} is ready", event.getJDA().getSelfUser().getAsTag());
     }
 }
